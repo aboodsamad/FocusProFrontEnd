94eee0564ea76d1ce8dd00f820d56442a3625d0d
Ahmad Hammoud <hammoudahmad530@gmail.com>
2026-04-17 15:31:13 +0300

    hi uio

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2961725250"
+    serviceWorkerVersion: "4291778592"
   }
 });
