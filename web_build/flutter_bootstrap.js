24d92017ce3bcafc23002a26fb286e609f5cd896
Abdelrahman El Samad <160246076+aboodsamad@users.noreply.github.com>
2026-04-21 00:50:30 +0300

    fix build error

--- web_build/flutter_bootstrap.js
+++ web_build/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1304553623"
+    serviceWorkerVersion: "3067291523"
   }
 });
