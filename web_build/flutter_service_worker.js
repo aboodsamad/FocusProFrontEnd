8f982a819a06a37d840e73aa3df7d69636131abd
Abdelrahman El Samad <160246076+aboodsamad@users.noreply.github.com>
2026-04-23 22:02:54 +0300

    FocusPro v1.5 - fix games and homepagedesign

--- web_build/flutter_service_worker.js
+++ web_build/flutter_service_worker.js
@@ -145,7 +145,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "c8648ea477a11188acf6f29e696e434e
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "8f370ad554e22f8566f2b22d0d39271b",
+"flutter_bootstrap.js": "00aa261a0bb183c14a36c87f658b1430",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
