7479fe8de95d14949f866fc7d8114e9e277806a5
Abdelrahman El Samad <160246076+aboodsamad@users.noreply.github.com>
2026-04-30 14:25:19 +0300

    just for build

--- web_build/flutter_service_worker.js
+++ web_build/flutter_service_worker.js
@@ -145,7 +145,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "c8648ea477a11188acf6f29e696e434e
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "6e8fe1fe3b1acbe4718c6bf9e7643751",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "d59eae5b51391d4e8c51f0ce16674173",
+"flutter_bootstrap.js": "ef2cf1d7c02676e0fd73871f1213aae0",
 "icons/Icon-192.png": "ebbeee8d6120468b5491b3ed07c7ab9a",
 "icons/Icon-512.png": "da6f607fa64e85273ab4d79100dfd5fb",
 "icons/Icon-maskable-192.png": "ebbeee8d6120468b5491b3ed07c7ab9a",
