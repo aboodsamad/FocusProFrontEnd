aaf7375b7ccd7b2b84f0952b5808f077ecc1d33b
ahmad-c-hub <hammoudahmad530@gmail.com>
2026-04-30 22:21:57 +0300

    hi abd

--- lib/features/home/pages/home_page.dart
+++ lib/features/home/pages/home_page.dart
@@ -1377,7 +1377,7 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
         );
       },
     );
-  
+  }
 }
 
 // ═══════════════════════════════════════════════════════════════════════════════
