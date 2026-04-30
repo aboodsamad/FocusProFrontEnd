e8eab6873b4ea485f4f64f3d97c4a10b7a454207
Abdelrahman El Samad <160246076+aboodsamad@users.noreply.github.com>
2026-05-01 00:19:08 +0300

    fix the build

--- web_build/flutter_service_worker.js
+++ web_build/flutter_service_worker.js
@@ -145,7 +145,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "c8648ea477a11188acf6f29e696e434e
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "6e8fe1fe3b1acbe4718c6bf9e7643751",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "ffeaaed683e0f60f72deacdfed3ae2be",
+"flutter_bootstrap.js": "2a9b4e67e81c85dffab6e80cf19e6ab8",
 "icons/Icon-192.png": "ebbeee8d6120468b5491b3ed07c7ab9a",
 "icons/Icon-512.png": "da6f607fa64e85273ab4d79100dfd5fb",
 "icons/Icon-maskable-192.png": "ebbeee8d6120468b5491b3ed07c7ab9a",
